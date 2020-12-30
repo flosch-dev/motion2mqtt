d92f91961a82013f593efc212dc55443c0f9cc18
Florian Schilling <flosch.devel@gmail.com>
2020-12-30 15:10:21 +0100

    handle connection error

--- motion2mqtt.py
+++ motion2mqtt.py
@@ -48,36 +48,68 @@ def detect_motion(mqttclient):
     function to detect motion and publish via mqtt connection
     loop every 100ms
     """
+    time.sleep(3)
     pir = MotionSensor(GPIO)
     while True:
         if pir.motion_detected:
             mqttclient.publish("CHANNEL/Motion/" + SENSOR,
-                                payload="motion")
+                                payload="motion",
+                                qos=1,
+                                retain=True)
             mqttclient.publish("CHANNEL/Motion/state",
-                                payload=SENSOR + " motion")
+                                payload=SENSOR + " motion",
+                                qos=1,
+                                retain=True)
             time.sleep(2)
             mqttclient.publish("CHANNEL/Motion/" + SENSOR,
-                                payload="noMotion")
+                                payload="noMotion",
+                                qos=1,
+                                retain=True)
             mqttclient.publish("CHANNEL/Motion/state",
-                                payload=SENSOR + " noMotion")
+                                payload=SENSOR + " noMotion",
+                                qos=1,
+                                retain=True)
+
         time.sleep(0.1)
 
 def on_disconnect(client, userdata,rc=0):
     print("DisConnected result code "+str(rc))
-    client.loop_stop()
-    exit(1)
+    client.connected_flag = False
+    client.disconnect_flag=True
+
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        client.connected_flag = True
+        print("connected to MQTT Server")
+    else:
+        print("Bad connection Returned code=",rc)
+        client.bad_connection_flag = True
 
 def main():
     """
     Main function
     """
+    mqtt.Client.connected_flag = False
+    mqtt.Client.bad_connection_flag = False
     mqttclient = mqtt.Client(MQTT_CLIENT_ID)
+    mqttclient.on_connect =  on_connect
+    mqttclient.on_disconnect = on_disconnect
+    mqttclient.loop_start()
+
+    print("connecting to MQTT Server: " + MQTT_HOST + ":" + str(MQTT_PORT) + "...")
     try:
         mqttclient.connect(MQTT_HOST, port=MQTT_PORT, keepalive=60)
     except:
-        print("MQTT Connection error")
-    mqttclient.loop_start()
-    mqttclient.on_disconnect = on_disconnect
+        print("ERROR: can't connect to MQTT Server")
+        exit(1)
+
+    while not mqttclient.connected_flag and not mqttclient.bad_connection_flag:
+        time.sleep(1)
+
+    if mqttclient.bad_connection_flag:
+        mqttclient.loop_stop()
+        sys.exit()
+
     detect_motion(mqttclient)
 
 if __name__=="__main__":
